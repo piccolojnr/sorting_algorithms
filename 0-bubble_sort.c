a4910dfb4853ecf1b6f92dffff2be73cf7a962c6
piccolojnr <rahimdaud246@gmail.com>
2023-11-18 02:59:56 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,21 +8,17 @@ void bubble_sort(int *array, size_t size)
 {
 size_t i, j;
 int temp;
-int swapped = 0;
+
+if (array == NULL)
+return;
 for (j = 0; j < size - 1; j++)
 {
-swapped = 0;
 for (i = 0; i < size - j - 1; i++)
-{
 if (array[i] > array[i + 1])
 {
 temp = array[i];
 array[i] = array[i + 1];
 array[i + 1] = temp;
-swapped = 1;
 }
-}
-if (!swapped)
-break;
 }
 }
