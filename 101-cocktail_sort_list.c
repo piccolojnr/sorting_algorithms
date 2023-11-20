70b960eb97db7bd3fd0d97b59d0f478232df1a51
piccolojnr <rahimdaud246@gmail.com>
2023-11-20 16:04:24 +0000

    cocktail2

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,36 +1,40 @@
 #include "sort.h"
+void swap_values(listint_t **list, listint_t *second, listint_t *first);
+
 /**
  * swap_values - swaps two nodes (tmp1 and tmp2) in a doubly linked list
  * @list: header
- * @tmp1: node 1
- * @tmp2: node 2
+ * @second: node 1
+ * @first: node 2
  */
-void swap_values(listint_t **list, listint_t *tmp1, listint_t *tmp2)
+void swap_values(listint_t **list, listint_t *second, listint_t *first)
 {
-if (tmp1->next)
-tmp1->next->prev = tmp2;
-if (tmp2->prev)
-tmp2->prev->next = tmp1;
-tmp2->next = tmp1->next;
-tmp1->prev = tmp2->prev;
-tmp2->prev = tmp1;
-tmp1->next = tmp2;
-if (!tmp1->prev)
-*list = tmp1;
+if (second->next)
+second->next->prev = first;
+if (first->prev)
+first->prev->next = second;
+first->next = second->next;
+second->prev = first->prev;
+first->prev = second;
+second->next = first;
+if (!second->prev)
+*list = second;
 }
 /**
  * cocktail_sort_list - sorts a doubly linked list of integers in
  * @list: header
-*/
+ */
 void cocktail_sort_list(listint_t **list)
 {
 listint_t *curr, *tmp1, *tmp2;
 int swapped = 1;
+
+if (list == NULL || *list == NULL || (*list)->next == NULL)return;
+
 while (swapped)
 {
 swapped = 0;
-curr = *list;
-while (curr->next)
+for (curr = *list; curr->next; curr = curr->next)
 {
 if (curr->n > curr->next->n)
 {
@@ -41,12 +45,11 @@ print_list(*list);
 curr = tmp2;
 swapped = 1;
 }
-curr = curr->next;
 }
 if (!swapped)
 break;
 swapped = 0;
-while (curr->prev)
+for (; curr->prev; curr = curr->prev)
 {
 if (curr->n < curr->prev->n)
 {
@@ -57,7 +60,6 @@ print_list(*list);
 curr = tmp2;
 swapped = 1;
 }
-curr = curr->prev;
 }
 }
 }
---
+++ 102-counting_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+void counting_sort(int *array, size_t size)
+{
+    int max = array[0];
+    int min = array[0];
+    int i, j;
+    size_t k;
+    int *count = NULL;
+    int *sorted = NULL;
+    int *temp = NULL;
+    
+}
